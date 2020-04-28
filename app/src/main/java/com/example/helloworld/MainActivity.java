47c7bdd4e349f270fab1d741c6ab121de31542f3
songjialin <songjialin@50lion.com>
2020-04-28 17:03:45 +0800

    init

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.helloworld;
 
 import android.app.Application;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +17,20 @@ public class MainActivity extends AppCompatActivity {
         button.setText("hello world");
         String name = Thread.currentThread().getName();
         button.setOnClickListener(it -> {
-            // Timber.d(String.format("Hello, %s %s", name, name));
+            AlertDialog alertDialog1 = new AlertDialog.Builder(this)
+                    .setTitle("这是标题")//标题
+                    .setMessage("这是内容")//内容
+                    .setIcon(R.mipmap.ic_launcher)//图标
+                    .setNegativeButton(R.string.app_name, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Log.i("hello", "world");
+                        }
+                    })
+                    .create();
+            alertDialog1.show();
         });
+
         // setContentView(R.layout.activity_main);
         setContentView(button);
         Application application = getApplication();
