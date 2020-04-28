8d6098fa411eece09dbd4cf79f8ec5f01b4807c0
songjialin <songjialin@50lion.com>
2020-04-28 16:29:32 +0800

    init

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.helloworld;
 
+import android.app.Application;
 import android.os.Bundle;
-
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Button button = new Button(this);
+        button.setText("hello world");
+        String name = Thread.currentThread().getName();
+        button.setOnClickListener(it -> {
+            // Timber.d(String.format("Hello, %s %s", name, name));
+        });
+        // setContentView(R.layout.activity_main);
+        setContentView(button);
+        Application application = getApplication();
+        Bundle metaData = application.getApplicationInfo().metaData;
     }
 }
